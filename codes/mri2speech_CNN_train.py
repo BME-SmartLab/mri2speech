52c3734fdad76aa7543a9a6042908c9fce419890
csapot <csapot@tmit.bme.hu>
2020-01-16 11:41:36 +0100

    CNN and CNN-LSTM models

--- codes/mri2speech_CNN_train.py
+++ codes/mri2speech_CNN_train.py
@@ -17,7 +17,7 @@ import random
 import vocoder_LSP_sptk
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten
 from keras.callbacks import EarlyStopping, CSVLogger, ModelCheckpoint
 
 from sklearn.model_selection import train_test_split
@@ -163,7 +163,7 @@ for speaker in ['f1', 'f2', 'm1', 'm2']:
                 
                 print('n_frames_all: ', mri_size, 'mgc_size: ', mgc_size)
                     
-        mri[train_valid] = mri[train_valid][0 : mri_size].reshape(-1, n_width*n_height)
+        mri[train_valid] = mri[train_valid][0 : mri_size].reshape(-1, n_width, n_height, 1)
         mgc[train_valid] = mgc[train_valid][0 : mgc_size]
 
 
@@ -183,12 +183,17 @@ for speaker in ['f1', 'f2', 'm1', 'm2']:
 
     ### single training
     model = Sequential()
-    model.add(Dense(1000, input_dim=n_width*n_height, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1000, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1000, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1000, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1000, kernel_initializer='normal', activation='relu'))
+    model.add(Conv2D(8, (3, 3), activation='relu', input_shape=(n_width,n_height, 1), padding='same'))
+    model.add(MaxPooling2D((2,2), padding='same'))
+    model.add(Conv2D(16, (3,3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2,2), padding='same'))
+    model.add(Conv2D(32, (3,3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2,2), padding='same'))
+    model.add(Flatten())
+    model.add(Dense(500, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(500, kernel_initializer='normal', activation='relu'))
     model.add(Dense(n_mgc, kernel_initializer='normal', activation='linear'))
+
 
     model.compile(loss='mean_squared_error', optimizer='adam')
 
@@ -196,7 +201,7 @@ for speaker in ['f1', 'f2', 'm1', 'm2']:
 
 
     current_date = '{date:%Y-%m-%d_%H-%M-%S}'.format( date=datetime.datetime.now() )
-    model_name = 'models/MRI2SPEECH_FC-DNN_baseline_' + speaker + '_' + current_date
+    model_name = 'models/MRI2SPEECH_CNN_' + speaker + '_' + current_date
 
     print('starting training', speaker, current_date)
 
